045b5f7343a1ec805bc3ad14c56585a046b48c13
imkarthi16 <karthiveera@gmail.com>
2022-08-09 11:05:42 +0530

    Adding first page

---
+++ pages/news.js
@@ -0,0 +1,11 @@
+// example : our-DOMImplementation.com/news
+
+const NewsPage=()=>{
+    return(
+        <div>
+            <h1>This is News Page</h1>
+        </div>
+    )
+}
+
+export default NewsPage
