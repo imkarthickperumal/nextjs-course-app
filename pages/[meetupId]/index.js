428bfa2791d00b05dfb5e4a406d0e1ff5615682c
imkarthi16 <karthiveera@gmail.com>
2022-08-09 18:04:31 +0530

    params ssg & data fetching

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,14 +1,32 @@
-import MeetupDetail from '../../components/meetups/MeetupDetail';
+import MeetupDetail from "../../components/meetups/MeetupDetail";
 
 function MeetupDetails() {
   return (
     <MeetupDetail
-      image='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg'
-      title='First Meetup'
-      address='Some Street 5, Some City'
-      description='This is a first meetup'
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
+      title="First Meetup"
+      address="Some Street 5, Some City"
+      description="This is a first meetup"
     />
   );
+}
+
+export function getStaticProps(context) {
+  // fetch data for a single meetup
+  const meetupId = context.params.meetupId;
+  console.log(meetupId);
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+        id: "m1",
+        title: "First Meetup",
+        address: "Some Street 5, Some City",
+        description: "This is a first meetup",
+      },
+    },
+  };
 }
 
 export default MeetupDetails;
