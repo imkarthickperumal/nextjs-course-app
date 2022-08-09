cfddc2d107145c17e122f918d9e0517a44f44ea5
imkarthi16 <karthiveera@gmail.com>
2022-08-09 12:06:17 +0530

    Linking between pages using inbuilt next-Link

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,10 +1,20 @@
 // example : our-domain.com/news
+import Link from "next/link";
+import { Fragment } from "react";
 
 const NewsPage = () => {
   return (
-    <div>
+    <Fragment>
       <h1>This is News Page</h1>
-    </div>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-is-a-great-framework">
+            NextJs is a Great Framework
+          </Link>
+        </li>
+        <li>Something Else</li>
+      </ul>
+    </Fragment>
   );
 };
 
