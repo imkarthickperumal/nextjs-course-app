1710f205091c6a7ae1df7ee3965f5487c6566f6b
imkarthi16 <karthiveera@gmail.com>
2022-08-09 11:41:09 +0530

    Dynamic parameter using useRouter hooks and query method

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,11 +1,18 @@
 // example : our-domain.com/news/something-important
 
-const DetailsPage=()=>{
-    return(
-        <div>
-            <h1>This is DetailsPage</h1>
-        </div>
-    )
-}
+import { useRouter } from "next/router";
+
+const DetailsPage = () => {
+  const router = useRouter();
+  const newsId =  router.query.newsId;
+
+  // send a request to the backend API
+  // to fetch the news item with newsId
+  return (
+    <div>
+      <h1>This is DetailsPage</h1>
+    </div>
+  );
+};
 
 export default DetailsPage;
