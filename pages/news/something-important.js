f023c59771215c1754b37bda62bf2ca5ee67f392
imkarthi16 <karthiveera@gmail.com>
2022-08-09 11:26:17 +0530

    Adding Nested Path(Routes)

---
+++ pages/news/something-important.js
@@ -0,0 +1,11 @@
+// example : our-domain.com/news/something-important
+
+const DetailsPage=()=>{
+    return(
+        <div>
+            <h1>This is DetailsPage</h1>
+        </div>
+    )
+}
+
+export default DetailsPage;
