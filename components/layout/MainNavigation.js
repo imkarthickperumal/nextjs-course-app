273a7f53c9719d0bc4a252ed22623f6329e4523b
imkarthi16 <karthiveera@gmail.com>
2022-08-09 13:39:50 +0530

    _app.js File & layout wrapper

--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -9,10 +9,10 @@ function MainNavigation() {
       <nav>
         <ul>
           <li>
-            <Link to='/'>All Meetups</Link>
+            <Link href='/'>All Meetups</Link>
           </li>
           <li>
-            <Link to='/new-meetup'>Add New Meetup</Link>
+            <Link href='/new-meetup'>Add New Meetup</Link>
           </li>
         </ul>
       </nav>
