6ec15bfa9509145f5a26e97d1a2a012e240cbe27
imkarthi16 <karthiveera@gmail.com>
2022-08-10 10:42:56 +0530

    working with MongoDB

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,8 +1,20 @@
+import { MongoClient } from "mongodb";
 // /api/new-meetup
 // POST /api/new-meetup
-function handler(req, res) {
-  if (req.metthod === "POST") {
+async function handler(req, res) {
+  if (req.method === "POST") {
     const data = req.body;
-    const { title, img, address, description } = data;
+    // const { title, img, address, description } = data;
+    const client = await MongoClient.connect(
+      "mongodb+srv://learning:learning2022@cluster0.dxsmnxr.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+    const result = await meetupsCollection.insertOne(data);
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({message : 'Meetup inserted!'});
   }
 }
+export default handler;
