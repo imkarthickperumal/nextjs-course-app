1c1a8d449207f76a32357b03a258dea44f0bd454
imkarthi16 <karthiveera@gmail.com>
2022-08-10 12:07:05 +0530

    sending http requests to our api

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,20 +1,27 @@
-import { MongoClient } from "mongodb";
+import { MongoClient } from 'mongodb';
+
 // /api/new-meetup
 // POST /api/new-meetup
+
 async function handler(req, res) {
-  if (req.method === "POST") {
+  if (req.method === 'POST') {
     const data = req.body;
-    // const { title, img, address, description } = data;
+
     const client = await MongoClient.connect(
       "mongodb+srv://learning:learning2022@cluster0.dxsmnxr.mongodb.net/meetups?retryWrites=true&w=majority"
     );
     const db = client.db();
+
+    const meetupsCollection = db.collection('meetups');
+
     const result = await meetupsCollection.insertOne(data);
+
     console.log(result);
 
     client.close();
 
-    res.status(201).json({message : 'Meetup inserted!'});
+    res.status(201).json({ message: 'Meetup inserted!' });
   }
 }
+
 export default handler;
